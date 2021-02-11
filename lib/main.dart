dc20266ffd0dad895c567e167caccdf3123b9971
dispersik <korotkmax@gmail.com>
2021-02-11 20:49:32 +0300

    minor code cleaning

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,14 @@
-import 'package:eduroam_autologin/back/networkRoutine.dart';
-import 'package:eduroam_autologin/back/storageHelper.dart';
+import 'package:eduroam_autologin/back/network_routine.dart';
+import 'package:eduroam_autologin/back/storage_helper.dart';
 import 'package:eduroam_autologin/front/widgets/autologin_mode_button.dart';
 import 'package:eduroam_autologin/front/widgets/credential_form.dart';
 import 'package:eduroam_autologin/front/widgets/eduroam_title.dart';
 import 'package:eduroam_autologin/front/widgets/save_data_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:url_launcher/url_launcher.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:eduroam_autologin/front/ui_helper.dart';
 
 import 'back/globals.dart';
-import 'front/widgets/ConnectToEduroamButton.dart';
+import 'front/widgets/connect_to_eduroam_button.dart';
 
 void main() {
   runApp(MyApp());
@@ -38,8 +35,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  void _callSetState() => setState(() {});
-
   void _saveUserData() {
     saveData(loginController.text, passwordController.text).then((value) {
       valueChanged = false;
